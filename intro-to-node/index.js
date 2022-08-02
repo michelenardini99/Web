576b501da36c35b1aa1ba34c264320c31e70e9c1
Michele Nardini <michele.nardini3g@gmail.com>
2022-08-02 14:08:26 +0200

    push

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+var superheroes = require("superheroes");
+var mySuperheroesName = superheroes.random();
+
+console.log(mySuperheroesName);
+
+var supervillains = require("supervillains");
+var mySupervillainsName = supervillains.random();
+
+console.log(mySupervillainsName);
